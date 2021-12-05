d0b4af6299fe5bb1d717b1ef487e7dbba22dec67
Elliot McGonigal <elliot@elliotmcgonigal.com>
2021-12-04 21:44:55 -0800

    partial commit, still working on getting string to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+//Business Logic
+
+
 function beepBoop(input) {
-  return 0;
+  let numberString = "";
+  for(counter = 0; counter <= numberString;  counter++) {
+    if (counter !== '0') {
+      numberString = numberString.concat(", ")
+    }
+    numberString = numberString.concat(counter);
+  }
+  console.log(numberString)
 }
