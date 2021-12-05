ea14c89cc652a7d73e14d8040739327722ba7eab
Elliot McGonigal <elliot@elliotmcgonigal.com>
2021-12-04 22:39:46 -0800

    pass fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,10 @@ function beepBoop(input) {
   let numberString = "";
   for(counter = 0; counter <= input;  counter++) {
     counter = counter.toString();
-    if (counter.includes('1')) {
+    if (counter.includes('2')) {
+      numberString = numberString.concat(", Boop!");
+    }
+    else if (counter.includes('1')) {
       numberString = numberString.concat(", Beep!");
     }
     else {
