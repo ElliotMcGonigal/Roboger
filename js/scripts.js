fd2835a8615e075d20958e753864c6af223cfe29
Elliot McGonigal <elliot@elliotmcgonigal.com>
2021-12-04 23:24:24 -0800

    add user interface

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,34 @@ function beepBoop(input) {
   let numberString = "";
   for(counter = 0; counter <= input;  counter++) {
     counter = counter.toString();
-    if (counter.includes('2')) {
+    if (counter.includes('3')) {
+      numberString = numberString.concat(", Won't you be my neighbor?");
+    }
+    else if (counter.includes('2')) {
       numberString = numberString.concat(", Boop!");
     }
     else if (counter.includes('1')) {
       numberString = numberString.concat(", Beep!");
     }
-    else {
+    else{
       if (counter !== '0') {
         numberString = numberString.concat(", ")
       }
       numberString = numberString.concat(counter);
     }
   }
-  console.log(numberString)
+  return numberString;
 }
+
+
+
+//UI Logic
+
+$(document).ready(function() {
+  $("form#formNum").submit(function(event) {
+    event.preventDefault(); 
+    let usersInput = $("#userInput").val();
+    let outputString = beepBoop(usersInput);
+    $("ul#output").append("<li>",outputString,"</li>");
+  });
+});
